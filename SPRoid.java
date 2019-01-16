3cd36e6fdee24b23228ce7605784175b8dfe66b8
MgilyA <zq6295migly@gmail.com>
2019-01-16 12:57:59 +0900

    [update]window追加

--- SPRoid.java
+++ SPRoid.java
@@ -1,4 +1,4 @@
-public class Client {
+public class SPRoid {
 
 	public static void main(String[] args) {
 		
---
+++ src/application/FormController.java
@@ -0,0 +1,16 @@
+package src.application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FormController {
+	@FXML private TextField field_Name;
+	@FXML private Button button_Enter;
+	@FXML private Label label_Output;
+
+	@FXML public void onButtonClicked() {
+		label_Output.setText("Hello "+ field_Name.getText()+ " !!");
+	}
+}
